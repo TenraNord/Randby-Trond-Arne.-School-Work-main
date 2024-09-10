122098ae642e35a6033e09b0fcd657aac2b03e9b
TenraNord <126238697+TenraNord@users.noreply.github.com>
2024-09-10 12:30:03 +0200

    friendContactAppFinished

--- Randby-TrondArne-Vue/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ Randby-TrondArne-Vue/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,34 +1,43 @@
 <template>
   <li>
-    <h2>
-      {{ friend.name }}
-      <button @click="toggleDetails">
-        {{ detailsAreVisible ? "Hide" : "Show" }} Details
-      </button>
-      <ul v-if="detailsAreVisible">
-        <li><strong>Phone:</strong>{{ friend.phone }}</li>
-        <li><strong>Email:</strong>{{ friend.email }}</li>
-      </ul>
-    </h2>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
 <script>
 export default {
+  props: {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    emailAddress: { type: String, required: true },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ["toggle-favorite", "delete"],
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
